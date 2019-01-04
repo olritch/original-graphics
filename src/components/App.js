d783d4fcee8129f89cb34f64af427b4571f23b0f
Hieu Tran <hmtran.ae@gmail.com>
2019-01-04 09:33:15 -0500

    added navbar, footer and login to routing

--- src/components/App.js
+++ src/components/App.js
@@ -1,20 +1,21 @@
 import React from 'react';
+import { BrowserRouter, Route } from 'react-router-dom'
 import Navbar from './Navbar';
 import Footer from './Footer';
 import Login from './Login';
 
-class App extends React.Component {
-
-    render() {
-        return (
-            <div>
-                <Navbar />
-                <Footer />
-                <Login />
-            </div>
-        )
-    }
+const App = () => {
+    return (
+        <div>
+            <BrowserRouter>
+                <div>
+                    <Navbar />
+                    <Route path = '/login' exact component={Login} />
+                    <Footer />
+                </div>
+            </BrowserRouter>
+        </div>
+    )
 }
-  
-export default App;
 
+export default App;
