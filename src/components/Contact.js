0895518e0edeeecf5eb0d560f7aa25a42ca621e7
Hieu Tran <hmtran.ae@gmail.com>
2019-01-05 17:40:48 -0500

    contact us subheader and additional fields

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -8,17 +8,28 @@ class Contact extends Component {
                 <div className='ui container'>
                     <h1 style={{ fontSize: '40px' }} className='ui header'>
                         Contact Us
+                        <div className='sub header'>Questions, need some help or have feedback? Fill out the form below and we'll get back to you as soon as we can.</div>
                     </h1>
-                    <Form>
-                        <Form.Field>
-                            <label>First Name</label>
-                            <input placeholder='First Name' />
-                        </Form.Field>
-                        <Form.Field>
-                            <label>Last Name</label>
-                            <input placeholder='Last Name' />
-                        </Form.Field>
-                    </Form>
+                    <div className='ui message'>
+                        <Form>
+                            <Form.Field>
+                                <label>First Name</label>
+                                <input type='text' placeholder='First Name' />
+                            </Form.Field>
+                            <Form.Field>
+                                <label>Last Name</label>
+                                <input type='text' placeholder='Last Name' />
+                            </Form.Field>
+                            <Form.Field>
+                                <label>Email</label>
+                                <input type='text' placeholder='Email Address' />
+                            </Form.Field>
+                            <Form.Field>
+                                <label>Phone Number</label>
+                                <input type='tel' placeholder='Phone Number' />
+                            </Form.Field>
+                        </Form>
+                    </div>
                 </div>
             </div>
         )
