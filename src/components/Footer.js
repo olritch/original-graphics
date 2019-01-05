d98418b9fa5eb0eee6bbd01a1f5bd6ab895b7279
Hieu Tran <hmtran.ae@gmail.com>
2019-01-05 13:32:48 -0500

    added social media links in footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -21,9 +21,9 @@ class Footer extends Component {
                         <div className='three wide column'>
                                 <div className='ui inverted header small'>Services</div>
                                 <div className='ui inverted link list'>
-                                    <Link className='item' to='/'>Sitemap</Link>
-                                    <Link className='item' to='/'>Shop</Link>
+                                    <Link className='item' to='/'>Store</Link>
                                     <Link className='item' to='/'>Class</Link>
+                                    <Link className='item' to='/'>Instructor Info</Link>
                                 </div>
                         </div>
                         <div className='three wide column'>
@@ -33,12 +33,31 @@ class Footer extends Component {
                                 <Link className='item' to='/'>Help</Link>
                                 <Link className='item' to='/'>Terms</Link>
                                 <Link className='item' to='/'>Privacy</Link>
+                                <Link className='item' to='/'>Sitemap</Link>
                             </div>
                         </div>
-                        <div className='5 wide column'>
-                            Icons here
+                        <div className='seven wide column'>
+                            <div className='ui icon inverted menu'>
+                                <Link to='/' className='item'>
+                                    <i className='facebook icon'></i>
+                                </Link>
+                                <Link to='/' className='item'>
+                                    <i className='twitter icon'></i>
+                                </Link>
+                                <Link to='/' className='item'>
+                                    <i className='youtube icon'></i>
+                                </Link>
+                                <Link to='/' className='item'>
+                                    <i className='instagram icon'></i>
+                                </Link>
+                                <Link to='/' className='item'>
+                                    <i className='linkedin icon'></i>
+                                </Link>
+                            </div>
+                            <div className='ui inverted bottom attached huge header'>Follow Us</div>
                         </div>
                     </div>
+                    <div className='ui inverted header tiny'>Copyright &copy; 2019 Original Graphics Inc. All Rights Reserved.</div>
                 </div>
             </div>
         )
