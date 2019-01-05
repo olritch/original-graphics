2ca6f4730963ae706f09bed4dfc7ef6dac189cb9
Hieu Tran <hmtran.ae@gmail.com>
2019-01-05 11:34:28 -0500

    added more links in footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -9,23 +9,30 @@ class Footer extends Component {
             <div className='ui inverted vertical footer segment'>
                 <div className='ui container'>
                     <div className='ui stackable inverted divided equal height stackable grid'>
-                        <div className='two wide column'>
+                        <div className='three wide column'>
+                            <div className='ui inverted header small'>About Original Graphics</div>
                             <div className='ui inverted link list'>
-                            <Link className='item' to='/'>Sitemap</Link>
-                            <Link className='item' to='/'>Contact Us</Link>
+                            <Link className='item' to='/'>Company Info</Link>
+                            <Link className='item' to='/'>Gallery</Link>
                             <Link className='item' to='/'>Blog</Link>
-                            <Link className='item' to='/'>About Us</Link>
-                            <Link className='item' to='/'>Shop</Link>
+                            <Link className='item' to='/'>Calendar</Link>
                             </div>
                         </div>
-                        <div className='two wide column'>
-
+                        <div className='three wide column'>
+                                <div className='ui inverted header small'>Services</div>
+                                <div className='ui inverted link list'>
+                                    <Link className='item' to='/'>Sitemap</Link>
+                                    <Link className='item' to='/'>Shop</Link>
+                                    <Link className='item' to='/'>Class</Link>
+                                </div>
                         </div>
-                        <div className='two wide column'>
+                        <div className='three wide column'>
+                            <div className='ui inverted header small'>Help & Contact</div>
                             <div className='ui inverted link list'>
+                                <Link className='item' to='/'>Contact Us</Link>
+                                <Link className='item' to='/'>Help</Link>
                                 <Link className='item' to='/'>Terms</Link>
                                 <Link className='item' to='/'>Privacy</Link>
-                                <Link className='item' to='/'>Help</Link>
                             </div>
                         </div>
                         <div className='5 wide column'>
