173a1ca0c1be1c71e61e7b3b5f033d7d1414a172
Hieu Tran <hmtran.ae@gmail.com>
2019-01-06 16:57:57 -0500

    StoreList component for store link in footer

---
+++ src/components/Store/StoreList.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class StoreList extends Component {
+    render() {
+        return (
+            <div>StoreList</div>
+        )
+    }
+}
+
+export default StoreList;
