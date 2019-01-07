8ea04cda469f2c4d9acfea072a0c13a2cac4f51d
Hieu Tran <hmtran.ae@gmail.com>
2019-01-07 15:10:11 -0500

    createuser function in user services server

--- server/services/user.service.server.js
+++ server/services/user.service.server.js
@@ -1,3 +1,11 @@
 module.exports = function(app) {
+    const userModel = require('../models/user/user.model.server');
 
+    app.post('/api/user', createUser);
+
+    async function createUser(req, res) {
+        const newUser = req.body;
+        const data = await userModel.createUser(newUser);
+        res.json(data);
+    }
 }
