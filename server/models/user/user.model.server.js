df506e656b6794116fbf0d7224adb1b052b66f0c
Hieu Tran <hmtran.ae@gmail.com>
2019-01-07 14:49:24 -0500

    createUser mongoose function in user model server

--- server/models/user/user.model.server.js
+++ server/models/user/user.model.server.js
@@ -2,3 +2,40 @@ const mongoose = require('mongoose');
 const UserSchema = require('./user.schema.server');
 const UserModel = mongoose.model('UserModel', UserSchema);
 
+UserModel.createUser = createUser;
+UserModel.findUserById = findUserById;
+UserModel.findUserByCredentials = findUserByCredentials;
+UserModel.findUsers = findUsers;
+UserModel.findUserByUsername = findUserByUsername;
+UserModel.updateUser = updateUser;
+UserModel.deleteUser = deleteUser;
+
+function createUser(user) {
+    return UserModel.create(user);
+}
+
+function findUserById(uid) {
+
+}
+
+function findUserByCredentials(username, password) {
+
+}
+
+function findUsers() {
+
+}
+
+function findUserByUsername(username) {
+
+}
+
+function updateUser(uid, user) {
+
+}
+
+function deleteUser(uid) {
+
+}
+
+module.exports = UserModel;
