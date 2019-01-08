2f5401a237fb8e4991b3abf63844a764d316d777
Hieu Tran <hmtran.ae@gmail.com>
2019-01-08 13:36:53 -0500

    change findUserByUsername to findUser and have 2 if statements for both username and password or just username

--- server/services/user.service.server.js
+++ server/services/user.service.server.js
@@ -3,7 +3,7 @@ module.exports = function(app) {
 
     app.post('/api/register', createUser);
     app.get('/api/user/:uid', findUserById);
-    app.get('/api/user', findUserByUsername)
+    app.get('/api/user', findUser);
 
     async function createUser(req, res) {
         const newUser = req.body;
@@ -17,8 +17,15 @@ module.exports = function(app) {
         res.json(data);
     }
 
-    async function findUserByUsername(req, res) {
+    async function findUser(req, res) {
         const username = req.query['username'];
+        const password = req.query['password'];
+        if (username && password) {
+            const data = await userModel.findUserByCredentials(username, password);
+            res.json(data);
+            return;
+        }
+
         if (username) {
             const data = await userModel.findUserByUsername(username);
             res.json(data);
