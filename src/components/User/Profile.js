c560a6db0e68012eed2d7847db94aa8a330dfdb3
TSN Student <student@ulem.org>
2019-01-09 15:53:23 -0500

    profile page for user

--- src/components/User/Profile.js
+++ src/components/User/Profile.js
@@ -1,11 +1,68 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
+import { TextArea, Dropdown, Form } from 'semantic-ui-react';
 
 class Profile extends Component {
-    render() {
-        return (
-            <div>Profile</div>
-        )
+
+  commentTopics = [
+    {
+      text: 'Customer Service',
+      value: 'Customer Service'
+    },
+    {
+      text: 'Product Information',
+      value: 'Product Information'
+    },
+    {
+        text: 'Class Information',
+        value: 'Class Information'
+    },
+    {
+        text: 'Other',
+        value: 'Other'
     }
+  ];
+
+  render() {
+    return <div>
+        <div className="ui vertical left aligned segment">
+          <div className="ui container">
+            <h1 style={{ fontSize: '40px' }} className="ui header">
+              Profile
+              <div className="sub header" style={{ paddingTop: '10px' }}>
+                Complete the following inputs so that we can better tailor our suggestions for you!
+              </div>
+            </h1>
+            <div className="ui message">
+              <Form>
+                <Form.Field>
+                  <label>First Name</label>
+                  <input type="text" placeholder="First Name" />
+                </Form.Field>
+                <Form.Field>
+                  <label>Last Name</label>
+                  <input type="text" placeholder="Last Name" />
+                </Form.Field>
+                <Form.Field>
+                  <label>Email</label>
+                  <input type="text" placeholder="Email Address" />
+                </Form.Field>
+                <Form.Field>
+                  <label>Phone Number</label>
+                  <input type="tel" placeholder="Phone Number" />
+                </Form.Field>
+
+                <label><strong>Biography</strong></label>
+                <TextArea rows={5} placeholder="Tell us more" />
+                <div className="ui divider" />
+                <button style={{ paddingTop: '20px' }} className="ui fluid primary huge button">
+                  Submit
+                </button>
+              </Form>
+            </div>
+          </div>
+        </div>
+      </div>
+  }
 }
 
 export default Profile;
