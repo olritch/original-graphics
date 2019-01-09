a87f782706c3dc896b132a8cb3420f9f388cde8f
Hieu Tran <hmtran.ae@gmail.com>
2019-01-09 12:49:55 -0500

    calendar onChange function

--- src/components/Classes/ClassCalendar.js
+++ src/components/Classes/ClassCalendar.js
@@ -1,28 +1,29 @@
 import React, { Component } from 'react';
 import Calendar from 'react-calendar';
+import axios from 'axios';
 
 class ClassCalendar extends Component {
+  state = {
+    date: new Date()
+  }
 
-    state = {
-        date: new Date()
-    }
+  onChange = date => {
+    this.setState({
+      date
+    })
+    console.log(date);
+  }
 
-    onChange = (date) => {
-        this.setState({
-            date
-        });
-    }
-
-    render() {
-        return (
-            <div className='ui center aligned container segment'>
-                <Calendar
-                    onChange={this.onChange}
-                    value={this.state.date}
-                />
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div className="ui center aligned container segment">
+        <Calendar
+          onChange={this.onChange}
+          value={this.state.date}
+        />
+      </div>
+    )
+  }
 }
 
 export default ClassCalendar;
