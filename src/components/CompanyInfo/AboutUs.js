3f6f272f334be9a86d7f707153f128c3567d29d4
sheila mcdaniel <smcdaniel617@aol.com>
2019-01-09 09:43:32 -0500

    About Us

--- src/components/CompanyInfo/AboutUs.js
+++ src/components/CompanyInfo/AboutUs.js
@@ -1,17 +1,3 @@
-//import React, { Component } from 'react';
-
-// class AboutUs extends Component {
-    // render() {
-       // return (
-           // <div className="ui form"><textarea placeholder="Original Graphics aims to give you an exemplary customer service experience. We have been providing high quality photography services 
-           // since 2015. You tell us what you want and we make it our highest priority to fulfill your request." rows="3" style="resize:none"></textarea>
-           // </div>
-    //    )
-  //  }
-// }
-
-//export default AboutUs;
-
 import React from 'react'
 import { Form, TextArea } from 'semantic-ui-react'
 
