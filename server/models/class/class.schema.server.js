a708a29b267d7a06212cd63d103e7503f369f17f
Hieu Tran <hmtran.ae@gmail.com>
2019-01-09 14:38:21 -0500

    added title, description, proficiency and date to class schema

--- server/models/class/class.schema.server.js
+++ server/models/class/class.schema.server.js
@@ -2,7 +2,10 @@ const mongoose = require('mongoose');
 
 const ClassSchema = mongoose.Schema(
     {
-
+        title: { type: String, default: '' },
+        description: { type: String, default: '' },
+        proficiency: { type: String, default: ''},
+        date : { type: String }
     },
     { collection: 'class' }
 );
