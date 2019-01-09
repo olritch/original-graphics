0f6678bcbccbeeff076d854e7fb09d3252e9aab3
Hieu Tran <hmtran.ae@gmail.com>
2019-01-09 07:58:12 -0500

    added shop.service.server to make requests in client

--- server/app.js
+++ server/app.js
@@ -1,5 +1,6 @@
 module.exports = function(app) {
     require('./services/user.service.server')(app);
     require('./services/blog.service.server')(app);
+    require('./services/shop.service.server.js')(app);
     require('./models/models.server');
 };
