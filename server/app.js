15d1c980428ca160b52ff17f83c278ac0a0f3274
Hieu Tran <hmtran.ae@gmail.com>
2019-01-09 08:10:02 -0500

    added class.service.server to make requests in client

--- server/app.js
+++ server/app.js
@@ -1,5 +1,6 @@
 module.exports = function(app) {
     require('./services/user.service.server')(app);
     require('./services/blog.service.server')(app);
+    require('./services/class.service.server')(app);
     require('./models/models.server');
 };
