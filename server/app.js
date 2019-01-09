44f5607073f45173218a1d392754402ed8b3fffd
Hieu Tran <hmtran.ae@gmail.com>
2019-01-09 07:52:46 -0500

    added blog.service.server to make requests in client

--- server/app.js
+++ server/app.js
@@ -1,4 +1,5 @@
 module.exports = function(app) {
     require('./services/user.service.server')(app);
+    require('./services/blog.service.server')(app);
     require('./models/models.server');
 };
