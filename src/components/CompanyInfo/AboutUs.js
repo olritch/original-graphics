ed4b7d3c58e2b51a0db3751371bb5da23aa40dde
Hieu Tran <hmtran.ae@gmail.com>
2019-01-09 10:29:50 -0500

    aboutus page reformat

--- src/components/CompanyInfo/AboutUs.js
+++ src/components/CompanyInfo/AboutUs.js
@@ -1,26 +1,22 @@
-//import React, { Component } from 'react';
+import React, { Component } from 'react';
 
-// class AboutUs extends Component {
-    // render() {
-       // return (
-           // <div className="ui form"><textarea placeholder="Original Graphics aims to give you an exemplary customer service experience. We have been providing high quality photography services 
-           // since 2015. You tell us what you want and we make it our highest priority to fulfill your request." rows="3" style="resize:none"></textarea>
-           // </div>
-    //    )
-  //  }
-// }
-
-//export default AboutUs;
-
-import React from 'react'
-import { Form, TextArea } from 'semantic-ui-react'
-
-const TextAreaAutoHeight = () => (
-  <Form>
-      <div className='ui text container'>
-    <TextArea autoHeight placeholder='Original Graphics aims to give you an exemplary customer service experience. We have been providing high quality photography services since 2015. You tell us what you want and we make it our highest priority to fulfill your request.' />
+class AboutUs extends Component {
+    render() {
+      return (
+        <div>
+          <div className="ui divider" />
+          <div className='ui text container'>
+            <div className="ui large message">
+              Original Graphics aims to give you an exemplary customer service
+              experience. We have been providing high quality photography
+              services since 2015. <div>Tell us what you want and we make it
+              our highest priority to fulfill your request.</div>
+            </div>
+          </div>
+          <div className="ui divider" />
         </div>
-  </Form>
-)
+      )
+   }
+}
 
-export default TextAreaAutoHeight
+export default AboutUs;
