79950a3c61e94772fa36db32073fc0ddcc546b01
Hieu Tran <hmtran.ae@gmail.com>
2019-01-09 14:40:26 -0500

    cerateClass button and onChange to show class descriptions to right of calendar

--- src/components/Classes/ClassCalendar.js
+++ src/components/Classes/ClassCalendar.js
@@ -3,27 +3,43 @@ import Calendar from 'react-calendar';
 import axios from 'axios';
 
 class ClassCalendar extends Component {
+
   state = {
+    classes: [],
     date: new Date()
   }
 
-  onChange = date => {
-    this.setState({
-      date
-    })
-    console.log(date);
+  onChange = async date => {
+    const dateString = formatDateString(date);
+    console.log(dateString);
+
+    let res = await axios.get(`/api/class?date=${dateString}`)
+      console.log(res.data);
+      this.setState({
+        classes: res.data
+      })
+  }
+
+  createClass = async () => {
+    const course = {
+      date: formatDateString(this.state.date)
+    }
+    let res = await axios.post(`/api/class`, course);
+    console.log(res.data);
   }
 
   render() {
     return (
       <div className="ui center aligned container segment">
-        <Calendar
-          onChange={this.onChange}
-          value={this.state.date}
-        />
+        <div onClick={this.createClass} className='ui button'>Create Class</div>
+        <Calendar onChange={this.onChange} value={this.state.date} />
       </div>
     )
   }
+}
+
+const formatDateString =(date) => {
+  return `${date.getFullYear()}-${date.getMonth()}-${date.getDate()}`
 }
 
 export default ClassCalendar;
