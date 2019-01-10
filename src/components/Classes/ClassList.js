f97a9f4fca93c1f635c5d33789bedbbc9dbedb8e
sheila mcdaniel <smcdaniel617@aol.com>
2019-01-10 16:01:45 -0500

    added more text to page

--- src/components/Classes/ClassList.js
+++ src/components/Classes/ClassList.js
@@ -16,7 +16,7 @@ class ClassList extends Component {
                         </div>
                         
                         <div className="ui segment">
-                            <p>In this class you will learn camera basics as we explain how to use your camera correctly and take better pictures. It's easy, fun and the best way to start learning.</p>
+                            <p>In this class you will learn camera basics as we explain how to use your camera correctly and take better pictures. It's easy, fun and the best way to start learning. Understanding photography's basics is essential to making great images. Start with basic photographic principles and camera controls and expand to composition, color theory and issues of visual perception.</p>
                         </div>
 
                     </div>
@@ -34,7 +34,7 @@ class ClassList extends Component {
                         </div>
                         
                         <div className="ui segment">
-                            <p>You will dive deeper into photography building your skills, personal style and your own photography portfolio.</p>
+                            <p>You will dive deeper into photography building your skills, personal style and your own photography portfolio. Brush up on technical issues and camera use. Improve lighting, composition, approach to subject matter.</p>
                         </div>
                         
                     </div>
@@ -52,7 +52,7 @@ class ClassList extends Component {
                         </div>
                         
                         <div className="ui segment">
-                            <p>Explore our advanced level class you will learn how to control every detail of your photographs while shooting and editing.</p>
+                            <p>Explore our advanced level class you will learn how to control every detail of your photographs while shooting and editing. Learn to use your camera to better tell your intended story through exploring more complex camera techniques and thinking processes.</p>
                         </div>
                         
                     </div>
