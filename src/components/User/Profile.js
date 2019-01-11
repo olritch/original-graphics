e2d688c65945348bb01e63dd00b68e30d9c0e567
Hieu Tran <hmtran.ae@gmail.com>
2019-01-11 11:52:14 -0500

    added user state, onChangeInput, onProfileSubmit callback

--- src/components/User/Profile.js
+++ src/components/User/Profile.js
@@ -2,59 +2,94 @@ import React, { Component } from 'react'
 import { TextArea, Form } from 'semantic-ui-react';
 
 class Profile extends Component {
+  state = {
+    firstName: '',
+    lastName: '',
+    telephone: '',
+    email: '',
+    bio: '',
+    errors : {}
+  }
 
-  commentTopics = [
-    {
-      text: 'Customer Service',
-      value: 'Customer Service'
-    },
-    {
-      text: 'Product Information',
-      value: 'Product Information'
-    },
-    {
-        text: 'Class Information',
-        value: 'Class Information'
-    },
-    {
-        text: 'Other',
-        value: 'Other'
-    }
-  ];
+  onChangeInput = e => {
+    this.setState({
+      [e.target.name]: e.target.value
+    })
+  }
+
+  onProfileSubmit = async e => {
+    e.preventDefault();
+
+    // const { firstName, lastName, telephone, email, bio } = this.state;
+
+    console.log(this.state);
+  }
 
   render() {
-    return <div>
+    return (
+      <div>
         <div className="ui vertical left aligned segment">
           <div className="ui container">
             <h1 style={{ fontSize: '40px' }} className="ui header">
               Profile
               <div className="sub header" style={{ paddingTop: '10px' }}>
-                Complete the following inputs so that we can better tailor our suggestions for you!
+                Complete the following inputs so that we can better tailor our
+                suggestions for you!
               </div>
             </h1>
             <div className="ui message">
-              <Form>
+              <Form onSubmit={this.onProfileSubmit}>
                 <Form.Field>
                   <label>First Name</label>
-                  <input type="text" placeholder="First Name" />
+                  <input
+                    onChange={this.onChangeInput}
+                    name='firstName'
+                    type="text"
+                    placeholder="First Name"
+                  />
                 </Form.Field>
                 <Form.Field>
                   <label>Last Name</label>
-                  <input type="text" placeholder="Last Name" />
+                  <input
+                    onChange={this.onChangeInput}
+                    name='lastName'
+                    type="text"
+                    placeholder="Last Name"
+                  />
                 </Form.Field>
                 <Form.Field>
                   <label>Email</label>
-                  <input type="text" placeholder="Email Address" />
+                  <input
+                    onChange={this.onChangeInput}
+                    name='email'
+                    type="text"
+                    placeholder="Email Address"
+                  />
                 </Form.Field>
                 <Form.Field>
                   <label>Phone Number</label>
-                  <input type="tel" placeholder="Phone Number" />
+                  <input
+                    onChange={this.onChangeInput}
+                    name='telephone'
+                    type="tel"
+                    placeholder="Phone Number"
+                  />
                 </Form.Field>
 
-                <label><strong>Biography</strong></label>
-                <TextArea rows={5} placeholder="Tell us more" />
+                <label>
+                  <strong>Biography</strong>
+                </label>
+                <TextArea
+                  onChange={this.onChangeInput}
+                  name='bio'
+                  rows={5}
+                  placeholder="Tell us more"
+                />
                 <div className="ui divider" />
-                <button style={{ paddingTop: '20px' }} className="ui fluid primary huge button">
+                <button
+                  style={{ paddingTop: '20px' }}
+                  className="ui fluid primary huge button"
+                >
                   Submit
                 </button>
               </Form>
@@ -62,6 +97,7 @@ class Profile extends Component {
           </div>
         </div>
       </div>
+    )
   }
 }
 
