0e27d9580b0e91d3583cdd3bde117dee9ec64928
Hieu Tran <hmtran.ae@gmail.com>
2019-01-14 13:54:34 -0500

    added api requests for creating and getting blog comments

--- server/services/blog.service.server.js
+++ server/services/blog.service.server.js
@@ -1,3 +1,17 @@
 module.exports = function(app) {
     const blogModel = require('../models/blog/blog.model.server');
+
+    app.post('/api/blog', createComment);
+    app.get('/api/blog', getComments);
+
+    async function createComment(req, res) {
+        const comment = req.body;
+        const data = await blogModel.createComment(comment);
+        res.json(data);
+    };
+
+    async function getComments(req, res) {
+        const data = await blogModel.getComments();
+        res.json(data);
+    }
 }
