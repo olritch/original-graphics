a2558f3aaaafd7d965a394e0f55e7e367379cb35
Hieu Tran <hmtran.ae@gmail.com>
2019-01-14 13:54:13 -0500

    updated blog schema

--- server/models/blog/blog.schema.server.js
+++ server/models/blog/blog.schema.server.js
@@ -2,7 +2,10 @@ const mongoose = require('mongoose');
 
 const BlogSchema = mongoose.Schema(
     {
-
+        author: { type : String, default: '' },
+        dateCreated : { type: Date, default: Date.now },
+        content: { type: String, default: '' },
+        // Comment in response to another user
     },
     { collection: 'blog' }
 );
