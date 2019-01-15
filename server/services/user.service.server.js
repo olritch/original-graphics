4f9330981e84d4e8110035ea7f4f1e994c8ef6a4
Hieu Tran <hmtran.ae@gmail.com>
2019-01-15 10:39:23 -0500

    changed res.send to res.sendStatus

--- server/services/user.service.server.js
+++ server/services/user.service.server.js
@@ -81,7 +81,7 @@ module.exports = function (app) {
 
     function logout(req, res) {
         req.logOut();
-        res.send(200);
+        res.sendStatus(200);
     }
 
     function loggedIn(req, res) {
