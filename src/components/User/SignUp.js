8f1f324094a94225e003bcb813a32a72513e2f63
Hieu Tran <hmtran.ae@gmail.com>
2019-01-16 17:21:51 -0500

    position css files within public index.html

--- src/components/User/SignUp.js
+++ src/components/User/SignUp.js
@@ -1,157 +1,204 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
 import { Link, withRouter } from 'react-router-dom'
-import { Message, Button, Segment, Header, Modal } from 'semantic-ui-react';
-import Recaptcha from 'react-recaptcha';
-import axios from 'axios';
+import { Message, Button, Segment, Header, Modal } from 'semantic-ui-react'
+import Recaptcha from 'react-recaptcha'
+import axios from 'axios'
 
 class SignUp extends Component {
-
   state = {
-      isVerified: false,
-      username: '',
-      password: '',
-      verifyPassword: '',
-      errors: {}
-    };
+    isVerified: false,
+    username: '',
+    password: '',
+    verifyPassword: '',
+    errors: {}
+  }
 
-    recaptchaLoaded = () => {
-        console.log('recaptcha successfully loaded');
-    }
+  recaptchaLoaded = () => {
+    console.log('recaptcha successfully loaded')
+  }
 
-    verifyCallback = (response) => {
-        if(response) {
-            this.setState({
-                isVerified: true
-            })
-        }
-    }
-
-    goLogin = () => {
-      this.props.changeLogin(true);
-      this.props.changeSignup(false);
-    }
-
-    handleSubscribe = (e) => {
-        if (this.state.isVerified) {
-            // this.props.changeSignup(false);
-        } else {
-            alert('Please verify that you are human!');
-        }
-    }
-
-    onChange = (e) => {
+  verifyCallback = response => {
+    if (response) {
       this.setState({
-        [e.target.name]: e.target.value
+        isVerified: true
       })
     }
+  }
 
-    onSubmit = async e => {
-      e.preventDefault();
+  goLogin = () => {
+    this.props.changeLogin(true)
+    this.props.changeSignup(false)
+  }
 
-      const { username, password, verifyPassword } = this.state;
+  handleSubscribe = e => {
+    if (this.state.isVerified) {
+      // this.props.changeSignup(false);
+    } else {
+      alert('Please verify that you are human!')
+    }
+  }
 
-      if (username === '') {
-        this.setState({
-          errors: { username: 'Username is required' }
-        });
-        return;
-      }
+  onChange = e => {
+    this.setState({
+      [e.target.name]: e.target.value
+    })
+  }
 
-      if (password === '') {
-        this.setState({
-          errors: { password: 'Password is required' }
-        });
-        return;
-      }
+  onSubmit = async e => {
+    e.preventDefault()
 
-      if (verifyPassword === '') {
-        this.setState({
-          errors: { verifyPassword: 'Verify password is required' }
-        });
-        return;
-      }
+    const { username, password, verifyPassword } = this.state
 
-      if (verifyPassword !== password) {
-        this.setState({
-          errors: { match: 'Password and verify password are not a match' }
-        });
-        return;
-      }
-
-      // clear errors
+    if (username === '') {
       this.setState({
-        errors: {}
-      });
-
-      let res = await axios.get(`/api/user?username=${username}`);
-      if (!res.data) {
-        const user = { username, password };
-        // register the user
-        res = await axios.post('/api/register', user);
-        // navigate to profile
-        this.props.history.push({
-          pathname: `profile-input`,
-          state: { user: res.data }
-        });
-        this.props.changeSignup(false);
-      } else {
-        this.setState({
-          errors: {
-            match: 'Username is taken, please try another one'
-          }
-        });
-      }
-
+        errors: { username: 'Username is required' }
+      })
+      return
     }
 
-    render() {
-      const { errors } = this.state;
+    if (password === '') {
+      this.setState({
+        errors: { password: 'Password is required' }
+      })
+      return
+    }
 
-      return  <div className="column" style={{ padding: '15px 5px 5px 5px' }}>
-      <div style={{fontSize: '50px'}} className="ui grey center aligned huge header">
-        Original Graphics
-      </div><div className="column" style={{ padding: '20px 5px 5px 5px' }}>
+    if (verifyPassword === '') {
+      this.setState({
+        errors: { verifyPassword: 'Verify password is required' }
+      })
+      return
+    }
+
+    if (verifyPassword !== password) {
+      this.setState({
+        errors: { match: 'Password and verify password are not a match' }
+      })
+      return
+    }
+
+    // clear errors
+    this.setState({
+      errors: {}
+    })
+
+    let res = await axios.get(`/api/user?username=${username}`)
+    if (!res.data) {
+      const user = { username, password }
+      // register the user
+      res = await axios.post('/api/register', user)
+      // navigate to profile
+      this.props.history.push({
+        pathname: `profile-input`,
+        state: { user: res.data }
+      })
+      this.props.changeSignup(false)
+    } else {
+      this.setState({
+        errors: {
+          match: 'Username is taken, please try another one'
+        }
+      })
+    }
+  }
+
+  render() {
+    const { errors } = this.state
+
+    return (
+      <div className="column" style={{ padding: '15px 5px 5px 5px' }}>
+        <div className="column" style={{ padding: '20px 5px 5px 5px' }}>
           <Modal.Header className="ui black left aligned huge header">
             Join Original Graphics
             <h2 className="ui header grey left aligned">
-              The best place for all of your photography and videography
-              needs.
+              The best place for all of your photography and videography needs.
             </h2>
-            <div className='ui divider'></div>
+            <div className="ui divider" />
           </Modal.Header>
           <Modal.Header className="ui black left aligned header medium">
             Create your Original Graphics Account
           </Modal.Header>
           <Modal.Content>
-            {errors.match && (<Message size='small' attached negative
-              content={errors.match}/>)}
-            <form onSubmit={this.onSubmit} className="ui large form" style={{ minWidth: '300px' }}>
+            {errors.match && (
+              <Message size="small" attached negative content={errors.match} />
+            )}
+            <form
+              onSubmit={this.onSubmit}
+              className="ui large form"
+              style={{ minWidth: '300px' }}
+            >
               <div className="ui stacked segment">
                 <div className="field">
                   <div className="ui fluid left icon input">
-                    <input onChange={this.onChange} name="username" type="text" placeholder="Username" />
+                    <input
+                      onChange={this.onChange}
+                      name="username"
+                      type="text"
+                      placeholder="Username"
+                    />
                     <i aria-hidden="true" className="user icon" />
                   </div>
-                  {errors.username && (<Message size='small' attached negative content={errors.username}/>)}
+                  {errors.username && (
+                    <Message
+                      size="small"
+                      attached
+                      negative
+                      content={errors.username}
+                    />
+                  )}
                 </div>
                 <div className="field">
                   <div className="ui fluid left icon input">
-                    <input onChange={this.onChange} name="password" type="password" placeholder="Password" />
+                    <input
+                      onChange={this.onChange}
+                      name="password"
+                      type="password"
+                      placeholder="Password"
+                    />
                     <i aria-hidden="true" className="lock icon" />
                   </div>
-                  {errors.password && (<Message size='small' attached negative content={errors.password} />)}
+                  {errors.password && (
+                    <Message
+                      size="small"
+                      attached
+                      negative
+                      content={errors.password}
+                    />
+                  )}
                 </div>
                 <div className="field">
                   <div className="ui fluid left icon input">
-                    <input onChange={this.onChange} name="verifyPassword" type="password" placeholder="Confirm Password" />
+                    <input
+                      onChange={this.onChange}
+                      name="verifyPassword"
+                      type="password"
+                      placeholder="Confirm Password"
+                    />
                   </div>
-                  {errors.verifyPassword && (<Message size='small' attached negative content={errors.verifyPassword} />)}
+                  {errors.verifyPassword && (
+                    <Message
+                      size="small"
+                      attached
+                      negative
+                      content={errors.verifyPassword}
+                    />
+                  )}
                 </div>
-                <Button onClick={this.handleSubscribe} className="ui teal large fluid button">
+                <Button
+                  onClick={this.handleSubscribe}
+                  className="ui teal large fluid button"
+                >
                   Create Account
                 </Button>
                 <Segment>
-                  <Recaptcha sitekey="6LfiA4cUAAAAAIPWf25xQ1bAohV7SRFQ4rS-aSLc" render="explicit" size="normal" theme="light" onloadCallback={this.recaptchaLoaded} verifyCallback={this.verifyCallback} />
+                  <Recaptcha
+                    sitekey="6LfiA4cUAAAAAIPWf25xQ1bAohV7SRFQ4rS-aSLc"
+                    render="explicit"
+                    size="normal"
+                    theme="light"
+                    onloadCallback={this.recaptchaLoaded}
+                    verifyCallback={this.verifyCallback}
+                  />
                 </Segment>
               </div>
             </form>
@@ -164,8 +211,9 @@ class SignUp extends Component {
             </Header>
           </div>
         </div>
-        </div>
-    }
+      </div>
+    )
+  }
 }
 
 export default withRouter(SignUp)
