64fba1431ef9078d5a189417418eac58753120bd
sheila mcdaniel <smcdaniel617@aol.com>
2019-01-17 15:14:27 -0500

    added photos to family page

--- src/components/Gallery/Family.js
+++ src/components/Gallery/Family.js
@@ -1,16 +1,51 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 
 class Family extends Component {
   render() {
-    return  (
+    return (
+      <div className="column" style={{ padding: "15px 5px 5px 5px" }}>
+        <div
+          style={{ fontSize: "50px" }}
+          className="ui grey center aligned huge header"
+        >
+          Original Graphics
+        </div>
 
-      <div className="column" style={{ padding: '15px 5px 5px 5px' }}>
-  <div style={{fontSize: '50px'}} className="ui grey center aligned huge header">
-    Original Graphics
-  </div>
-      <div>Family</div>
+        <div className="ui vertical center aligned segment">
+          <h1 style={{ fontSize: "40px" }} className="ui header">
+            Family
+          </h1>
+        </div>
+
+        <div>
+          <div class="ui medium images">
+         
+            <img
+              className="ui image"
+              alt="family"
+              src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTY859d2HZzcc_fO-hZ0Mf-FS_X1Rd0pls6kMOh2UqX20sJKVOzEA"
+            />
+          </div>
+        </div>
+
+        <div className="ui medium images">
+          <img
+            className="ui image"
+            alt="family"
+            src="https://www.condoncharles.com.au/wp-content/uploads/2016/06/family-pictures-19-300x200.jpg"
+          />
+        </div>
+
+        <div className="ui medium images">
+          <img
+            className="ui image"
+            alt="family"
+            src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRrmLBG2qYPoS31N_Ht10D2uIw2ucbQATvHMbfe6x3eyDciJg99ZA"
+          />
+        </div>
       </div>
-  )
+      
+    );
+  }
 }
-}
-export default Family
+export default Family;
--- src/components/Gallery/Wedding.js
+++ src/components/Gallery/Wedding.js
@@ -1,17 +1,44 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 
 class Wedding extends Component {
   render() {
-    return  (
+    return (
+      <div className="column" style={{ padding: "15px 5px 5px 5px" }}>
+        <div
+          style={{ fontSize: "50px" }}
+          className="ui grey center aligned huge header"
+        >
+          Original Graphics
+        </div>
+        <div className="ui horizontal center aligned segment">
+          <h1 style={{ fontSize: "40px" }} className="ui header">
+            Family
+          </h1>
+        </div>
+        <div>
+          <div className="ui three column grid">
+            <img
+              className="ui image"
+              alt="family"
+              src="https://cdn.essensedesigns.com/wp-content/uploads/2018/05/D2457-1-530x845.jpg"
+            />
+          </div>
+        </div>
 
-      <div className="column" style={{ padding: '15px 5px 5px 5px' }}>
-  <div style={{fontSize: '50px'}} className="ui grey center aligned huge header">
-    Original Graphics
-  </div>
-      <div>Wedding</div>
+        <div className="ui three column grid">
+          <img
+            className="ui image"
+            alt="family"
+            src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRL4BzgIgpOg93-cRpvxJT823-JQuj5GpUpJKAHbUkIxM8j_6fd"
+          />
+        </div>
+
+        <div className="ui three column grid">
+          <img className="ui image" alt="family" src="http://www.webdesignjoy.com/wp-content/uploads/2017/09/wedding-Photography-17.jpg" />
+        </div>
       </div>
-  )
-}
+    );
+  }
 }
 
-export default Wedding
+export default Wedding;
