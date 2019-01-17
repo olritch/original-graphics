437f0ed57956b093b3d11c667d83899593b17f7a
Hieu Tran <hmtran.ae@gmail.com>
2019-01-17 11:48:55 -0500

    user interests add and delete is working

--- src/components/User/Profile.js
+++ src/components/User/Profile.js
@@ -1,153 +1,167 @@
 // If have time, implement a friends list for user based on people they took the class with
 
-import React, { Component } from 'react'
-import { Dropdown, Message } from 'semantic-ui-react'
-import axios from 'axios'
+import React, { Component } from "react";
+import { Dropdown, Message } from "semantic-ui-react";
+import axios from "axios";
 
 function formatDate(date) {
-  return date.substring(0, 4)
+  return date.substring(0, 4);
 }
 
 class Profile extends Component {
   state = {
     user: {
-      dateCreated: '',
-      classes: []
+      dateCreated: "",
+      classes: [],
+      interests: []
     },
-    showInfo: '',
+    showInfo: "",
     interests: [],
     errors: {},
-    isLoggedIn: false
-  }
+    isLoggedIn: false,
+    newInterests: []
+  };
 
   options = [
     {
-      key: 'food-photography',
-      value: 'Food Photography',
-      text: 'Food Photography'
+      key: "food-photography",
+      value: "Food Photography",
+      text: "Food Photography"
     },
     {
-      key: 'nature-photography',
-      value: 'Nature Photography',
-      text: 'Nature Photography'
+      key: "nature-photography",
+      value: "Nature Photography",
+      text: "Nature Photography"
     },
     {
-      key: 'landscape-photography',
-      value: 'Landscape Photography',
-      text: 'Landscape Photography'
+      key: "landscape-photography",
+      value: "Landscape Photography",
+      text: "Landscape Photography"
     },
     {
-      key: 'wedding-photography',
-      value: 'Wedding Photography',
-      text: 'Wedding Photography'
+      key: "wedding-photography",
+      value: "Wedding Photography",
+      text: "Wedding Photography"
     },
     {
-      key: 'aerial-photography',
-      value: 'Aerial Photography',
-      text: 'Aerial Photography'
+      key: "aerial-photography",
+      value: "Aerial Photography",
+      text: "Aerial Photography"
     },
     {
-      key: 'family-photography',
-      value: 'Family Photography',
-      text: 'Family Photography'
+      key: "family-photography",
+      value: "Family Photography",
+      text: "Family Photography"
     },
     {
-      key: 'pet-photography',
-      value: 'Pet Photography',
-      text: 'Pet Photography'
+      key: "pet-photography",
+      value: "Pet Photography",
+      text: "Pet Photography"
     },
     {
-      key: 'videography',
-      value: 'Videography',
-      text: 'Videography'
+      key: "videography",
+      value: "Videography",
+      text: "Videography"
     },
     {
-      key: 'photoshop',
-      value: 'Photoshop',
-      text: 'Photoshop'
+      key: "photoshop",
+      value: "Photoshop",
+      text: "Photoshop"
     }
-  ]
+  ];
 
   componentDidMount = async () => {
-    let user = await this.props.isLoggedIn()
+    let user = await this.props.isLoggedIn();
     if (user.data) {
       this.setState({
         user: user.data,
         isLoggedIn: true
-      })
+      });
     } else {
-      this.props.history.push('/')
+      this.props.history.push("/");
     }
 
     let userClasses = await axios.get(
       `/api/userClasses?uid=${this.state.user._id}`
-    )
+    );
     this.setState({
       user: userClasses.data,
       interests: userClasses.data.interests
-    })
-  }
+    });
+  };
 
   onInfoClick = e => {
     this.setState({
       showInfo: e.target.id
-    })
-  }
+    });
+  };
 
   onDropdownInputChange = async (e, data) => {
+    this.setState({
+      errors: {},
+      newInterests: data.value
+    });
+
     await axios.put(
-      `/api/interests?uid=${this.state.user._id}&interests=${
+      `/api/interests/add?uid=${this.state.user._id}&interests=${
         data.value.slice(-1)[0]
       }`
-    )
+    );
 
-    if (this.state.interests.includes(data.value.slice(-1)[0] === false)) {
+    if (!this.state.interests.includes(data.value.slice(-1)[0])) {
       this.setState({
         interests: [...this.state.interests, data.value.slice(-1)[0]]
-      })
+      });
       return;
     }
 
     this.setState({
-      errors: { interestExist: "You've already added this interest." }
-    })
-  }
+      errors: { interestExist: "You've already added this interest." },
+      newInterests: []
+    });
+  };
 
-  addInterest = async () => {
+  addInterest = () => {
+    let user = { ...this.state.user };
+    user.interests = this.state.interests;
+    this.setState({
+      user,
+      newInterests: []
+    });
+  };
 
-  }
+  deleteInterest = async currentInterest => {
+    let user = { ...this.state.user };
+    user.interests = this.state.user.interests.filter(
+      interest => interest !== currentInterest
+    );
 
-  deleteInterest = async i => {
-    console.log(i)
-  }
+    await axios.put(
+      `/api/interests/remove?uid=${user._id}&interests=${
+      currentInterest
+      }`
+    )
 
-  addReminder = async () => {
+    this.setState({
+      user,
+      interests: user.interests
+    });
+  };
 
-  }
+  addReminder = async () => {};
 
-  deleteReminder = async () => {
-
-  }
+  deleteReminder = async () => {};
 
   render() {
-    // console.log(this.state.user)
-    const {
-      firstName,
-      lastName,
-      bio,
-      email,
-      telephone,
-      classes,
-      dateCreated
-    } = this.state.user
+    const { firstName, lastName, bio, email, telephone, classes, dateCreated } = this.state.user;
 
-    const { errors } = this.state
+    const { errors } = this.state;
 
     return (
       <div>
-        <div className="column" style={{ padding: '15px 5px 75px 5px' }}>
+        <div className="column" style={{ padding: "15px 5px 75px 5px" }}>
           <div
-            style={{ fontSize: '50px' }}
+            style={{ fontSize: "50px" }}
             className="ui grey center aligned huge header"
           >
             Original Graphics
@@ -175,7 +189,7 @@ class Profile extends Component {
                 <div className="description">
                   <div>
                     <i
-                      style={{ marginRight: '20px' }}
+                      style={{ marginRight: "20px" }}
                       className="envelope icon"
                     />
                     {email}
@@ -183,14 +197,14 @@ class Profile extends Component {
                 </div>
                 <div className="description">
                   <div>
-                    <i style={{ marginRight: '20px' }} className="phone icon" />
+                    <i style={{ marginRight: "20px" }} className="phone icon" />
                     {telephone}
                   </div>
                 </div>
                 <div className="description">
                   <div>
                     <i
-                      style={{ marginRight: '20px' }}
+                      style={{ marginRight: "20px" }}
                       className="info circle icon"
                     />
                     {bio}
@@ -201,14 +215,14 @@ class Profile extends Component {
             <div className="ui divider" />
             <div className="ui vertical buttons">
               <button
-                style={{ width: '210px', marginBottom: '10px' }}
+                style={{ width: "210px", marginBottom: "10px" }}
                 className="ui large inverted blue button"
               >
                 <i className="calendar alternate icon" />
                 My Events
               </button>
               <button
-                style={{ width: '210px', marginBottom: '10px' }}
+                style={{ width: "210px", marginBottom: "10px" }}
                 className="ui large inverted blue button"
               >
                 <i className="camera icon" />
@@ -226,40 +240,41 @@ class Profile extends Component {
                   content={errors.interestExist}
                 />
               )}
-              <form style={{ paddingBottom: '10px' }} className="ui form">
+              <form style={{ paddingBottom: "10px" }} className="ui form">
                 <div className="field">
                   <Dropdown
                     placeholder="Interests"
                     fluid
+                    name="test"
                     multiple
                     selection
                     options={this.options}
                     onChange={this.onDropdownInputChange}
+                    value={this.state.newInterests}
                   />
+                </div>
+                <div
+                  onClick={this.addInterest}
+                  style={{ width: "150px" }}
+                  className="ui inverted blue button"
+                >
+                  Add
                 </div>
               </form>
 
-              {this.state.interests.map((interest, i) => (
+              {this.state.user.interests.map((interest, i) => (
                 <div
-                  style={{ marginBottom: '10px', marginRight: '5px' }}
-                  className="ui label"
+                  style={{ marginBottom: "10px", marginRight: "5px" }}
+                  className="ui large label"
                   key={i}
                 >
                   {interest}
                   <i
-                    onClick={this.deleteInterest.bind(null, i)}
+                    onClick={this.deleteInterest.bind(this, interest)}
                     className="red delete icon"
                   />
                 </div>
               ))}
-
-              <div
-                onClick={this.addInterest}
-                style={{ width: '150px' }}
-                className="ui inverted blue button"
-              >
-                Add
-              </div>
             </div>
 
             <div className="ui divider" />
@@ -268,26 +283,38 @@ class Profile extends Component {
             <div className="ui segment">
               <div className="ui header">My Reminders</div>
               <div
-                style={{ paddingBottom: '15px' }}
+                style={{ paddingBottom: "15px" }}
                 className="ui fluid input focus"
               >
                 <input type="text" placeholder="I need to..." />
               </div>
               <button
-                onCLick={this.addReminder}
+                onClick={this.addReminder}
                 className="ui labeled icon button"
               >
                 <i className="pencil icon" /> Post
               </button>
             </div>
-            <div className='ui segment'>
-                <div className='ui large header center aligned'>Our Recommended Blogs and Articles Based on Your Interests</div>
+            <div className="ui segment">
+              <div className="ui large header center aligned">
+                Our Recommended Blogs and Articles Based on Your Interests
+              </div>
+              {this.state.user.interests.length ? (
+                <div />
+              ) : (
+                <Message
+                  size="small"
+                  attached
+                  negative
+                  content="Please update your interests to see our recommended list"
+                />
+              )}
             </div>
           </div>
           <div className="five wide column">
-            <div style={{ marginBottom: '10px' }} className="ui segment">
+            <div style={{ marginBottom: "10px" }} className="ui segment">
               <div
-                style={{ paddingBottom: '10px' }}
+                style={{ paddingBottom: "10px" }}
                 className="ui center aligned header"
               >
                 Upcoming Classes:
@@ -296,12 +323,14 @@ class Profile extends Component {
                 return (
                   <div className="ui segment" key={i}>
                     <div className="ui small center aligned header">
-                      <span><strong>{course.title}</strong></span>
+                      <span>
+                        <strong>{course.title}</strong>
+                      </span>
                     </div>
-                    <div style={{ textAlign: 'center' }}>
+                    <div style={{ textAlign: "center" }}>
                       <strong>{course.proficiency}</strong>
                     </div>
-                    <div style={{ paddingBottom: '25px', textAlign: 'center' }}>
+                    <div style={{ paddingBottom: "25px", textAlign: "center" }}>
                       <strong>{course.date}</strong>
                     </div>
                     {this.state.showInfo === course._id ? (
@@ -316,14 +345,14 @@ class Profile extends Component {
                       </button>
                     )}
                   </div>
-                )
+                );
               })}
             </div>
           </div>
         </div>
       </div>
-    )
+    );
   }
 }
 
-export default Profile
+export default Profile;
