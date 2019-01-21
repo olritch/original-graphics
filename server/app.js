a4dd37a2d757fe553c04869340da30e496bf0b37
Hieu Tran <hmtran.ae@gmail.com>
2019-01-21 15:52:49 -0500

    added reminder.service.server to app.js in server folder

--- server/app.js
+++ server/app.js
@@ -2,6 +2,7 @@ module.exports = function(app) {
     require('./services/user.service.server')(app);
     require('./services/blog.service.server')(app);
     require('./services/class.service.server')(app);
-    require('./services/shop.service.server.js')(app);
+    require('./services/shop.service.server')(app);
+    require('./services/reminder.service.server')(app);
     require('./models/models.server');
 };
