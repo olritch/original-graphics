1c8b2bf346d212e5b07de839be9c25d09264f702
Hieu Tran <hmtran.ae@gmail.com>
2019-01-21 15:55:26 -0500

    crud functions for reminder in model file

--- server/models/user/reminders/reminder.model.server.js
+++ server/models/user/reminders/reminder.model.server.js
@@ -3,7 +3,24 @@ const ReminderSchema = require('./reminder.schema.server');
 const ReminderModel = mongoose.model('ReminderModel', ReminderSchema);
 
 ReminderModel.createReminder = createReminder;
+ReminderModel.deleteReminder = deleteReminder;
+ReminderModel.updateReminder = updateReminder;
+ReminderModel.getReminders = getReminders;
 
-function createReminder() {
-
+function createReminder(reminder) {
+    return ReminderModel.create(reminder);
 }
+
+function deleteReminder(rid) {
+    return ReminderModel.remove({ _id : rid });
+}
+
+function updateReminder(rid, reminder) {
+    return ReminderModel.updateOne({ _id : rid }, reminder);
+}
+
+function getReminders() {
+    return ReminderModel.find({});
+}
+
+module.exports = ReminderModel;
