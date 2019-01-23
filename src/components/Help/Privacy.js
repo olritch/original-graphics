d7840cddf9d1a7ffb052b3a377821f5be47a0380
Olritch Germain <olritcholritch@gmail.com>
2019-01-23 11:12:02 -0500

    Added logo

--- src/components/Help/Privacy.js
+++ src/components/Help/Privacy.js
@@ -5,7 +5,17 @@ class Privacy extends Component {
         return (
         <div className='column' style={{padding: '15px 5px 5px 5px' }}>
             <div style={{ fontSize: '50px' }} className="ui grey center aligned huge header">
+            <img
+              src="https://image.shutterstock.com/display_pic_with_logo/179108744/718542784/stock-vector-initial-logo-letter-og-with-shield-and-crown-icon-golden-color-isolated-on-black-background-718542784.jpg"
+              style={{ width: "6%" }}
+              className="w3-round"
+            />
                 Original Graphics
+                <img
+              src="https://image.shutterstock.com/display_pic_with_logo/179108744/718542784/stock-vector-initial-logo-letter-og-with-shield-and-crown-icon-golden-color-isolated-on-black-background-718542784.jpg"
+              style={{ width: "6%" }}
+              className="w3-round"
+            />
             </div>
             <div className='ui vertical center aligned segment'>
                 <h1 style={{ fontSize: '40px' }} className="ui header">
