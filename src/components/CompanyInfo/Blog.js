9710fac410295ab63b57a787760ecce3be72c5a6
Hieu Tran <hmtran.ae@gmail.com>
2019-01-23 15:12:15 -0500

    added admin + user conditional rendering for blog posts

--- src/components/CompanyInfo/Blog.js
+++ src/components/CompanyInfo/Blog.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { Message } from 'semantic-ui-react';
+import { Message } from "semantic-ui-react";
 import axios from "axios";
 import { Link } from "react-router-dom";
 
@@ -8,7 +8,9 @@ class Blog extends Component {
     author: "",
     content: "",
     comments: [],
-    errors: {}
+    errors: {},
+    user: {},
+    isAdmin: false
   };
 
   componentDidMount = async () => {
@@ -19,12 +21,19 @@ class Blog extends Component {
     let user = await this.props.isLoggedIn();
     if (user.data) {
       this.setState({
-        author: `${user.data.firstName} ${user.data.lastName}`
+        author: `${user.data.firstName} ${user.data.lastName}`,
+        user: user.data
       });
     } else if (user.data === 0) {
       this.setState({
-        author: 'Anonymous'
-      })
+        author: "Anonymous"
+      });
+    }
+
+    if (user.data.admin === true) {
+      this.setState({
+        isAdmin: true
+      });
     }
   };
 
@@ -33,7 +42,7 @@ class Blog extends Component {
 
     this.setState({
       errors: {}
-    })
+    });
 
     const { author, content } = this.state;
 
@@ -42,19 +51,19 @@ class Blog extends Component {
       content
     };
 
-    if (content === '') {
+    if (content === "") {
       this.setState({
-        errors: {emptyField: 'Please provide a comment'}
-      })
-      return
+        errors: { emptyField: "Please provide a comment" }
+      });
+      return;
     }
 
     let res = await axios.post(`/api/blog`, comment);
     this.setState({
-      comments: [res.data, ...this.state.comments],
+      comments: [res.data, ...this.state.comments]
     });
-    
-    this.refs.comment.value = ''
+
+    this.refs.comment.value = "";
   };
 
   onInputChange = e => {
@@ -79,17 +88,17 @@ class Blog extends Component {
           style={{ fontSize: "50px" }}
           className="ui grey center aligned huge header"
         >
-        <img
-              src="https://image.shutterstock.com/display_pic_with_logo/179108744/718542784/stock-vector-initial-logo-letter-og-with-shield-and-crown-icon-golden-color-isolated-on-black-background-718542784.jpg"
-              style={{ width: "6%" }}
-              className="w3-round"
-            />
+          <img
+            src="https://image.shutterstock.com/display_pic_with_logo/179108744/718542784/stock-vector-initial-logo-letter-og-with-shield-and-crown-icon-golden-color-isolated-on-black-background-718542784.jpg"
+            style={{ width: "6%" }}
+            className="w3-round"
+          />
           Original Graphics
           <img
-              src="https://image.shutterstock.com/display_pic_with_logo/179108744/718542784/stock-vector-initial-logo-letter-og-with-shield-and-crown-icon-golden-color-isolated-on-black-background-718542784.jpg"
-              style={{ width: "6%" }}
-              className="w3-round"
-            />
+            src="https://image.shutterstock.com/display_pic_with_logo/179108744/718542784/stock-vector-initial-logo-letter-og-with-shield-and-crown-icon-golden-color-isolated-on-black-background-718542784.jpg"
+            style={{ width: "6%" }}
+            className="w3-round"
+          />
         </div>
 
         <div className="ui padded vertical center aligned segment">
@@ -98,8 +107,17 @@ class Blog extends Component {
           </h1>
         </div>
 
-        <div className='ui container'>
-        {this.state.errors.emptyField && <Message style={{marginTop: '10px'}} size='small' attached compact negative content={this.state.errors.emptyField}/>}
+        <div className="ui container">
+          {this.state.errors.emptyField && (
+            <Message
+              style={{ marginTop: "10px" }}
+              size="small"
+              attached
+              compact
+              negative
+              content={this.state.errors.emptyField}
+            />
+          )}
         </div>
 
         <div className="ui grid segment container">
@@ -107,10 +125,15 @@ class Blog extends Component {
             <div style={{ paddingBottom: "10px" }}>
               <div className="ui text container form">
                 <div className="field">
-                  <textarea rows='3' ref='comment' onChange={this.onInputChange} name="content" />
+                  <textarea
+                    rows="3"
+                    ref="comment"
+                    onChange={this.onInputChange}
+                    name="content"
+                  />
                 </div>
                 <div
-                  style={{marginBottom: '20px'}}
+                  style={{ marginBottom: "20px" }}
                   onClick={this.createComment}
                   className="ui fluid large teal button"
                 >
@@ -126,19 +149,28 @@ class Blog extends Component {
                         {comment.author}
                       </Link>
                       <div className="metadata">
-                        <span className="date">{formatDate(comment.dateCreated)}</span>
+                        <span className="date">
+                          {formatDate(comment.dateCreated)}
+                        </span>
                       </div>
-                      <div className="text">{comment.content}</div>
+                      <div style={{paddingBottom: '10px'}} className="text">{comment.content}</div>
                       <div className="actions">
                         <button className="ui small basic button reply">
                           Reply
                         </button>
-                        <div
-                          onClick={this.deleteComment.bind(null, comment)}
-                          className="ui mini red basic button"
-                        >
-                          Delete
-                        </div>
+
+                        {this.state.isAdmin ||
+                        this.state.user.firstName ===
+                          comment.author.split(" ")[0] ? (
+                          <div
+                            onClick={this.deleteComment.bind(null, comment)}
+                            className="ui mini red basic button"
+                          >
+                            Delete
+                          </div>
+                        ) : (
+                          <span />
+                        )}
                       </div>
                     </div>
                     <div className="ui divider" />
@@ -155,12 +187,12 @@ class Blog extends Component {
 
 function formatDate(timeNow) {
   timeNow = new Date();
-  let hours   = timeNow.getHours();
+  let hours = timeNow.getHours();
   let minutes = timeNow.getMinutes();
-  let timeString = "" + ((hours > 12) ? hours - 12 : hours);
-  timeString  += ((minutes < 10) ? ":0" : ":") + minutes;
-  timeString  += (hours >= 12) ? " P.M." : " A.M.";
-  return `${new Date().toDateString()} ${timeString}`
+  let timeString = "" + (hours > 12 ? hours - 12 : hours);
+  timeString += (minutes < 10 ? ":0" : ":") + minutes;
+  timeString += hours >= 12 ? " P.M." : " A.M.";
+  return `${new Date().toDateString()} ${timeString}`;
 }
 
 export default Blog;
