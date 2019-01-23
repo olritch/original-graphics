bd5d91806644e4bdd329143fa155e5d3e0ee9a88
Olritch Germain <olritcholritch@gmail.com>
2019-01-23 15:32:36 -0500

    build for prod

--- server/models/models.server.js
+++ server/models/models.server.js
@@ -1,10 +1,21 @@
 var connectionString = "mongodb://127.0.0.1:27017/original-graphics"; // for local
 
-var mongoose = require('mongoose');
+if (process.env.MLAB_USERNAME_WEBDEV) {
+  // check if running remotely
 
+  var username = process.env.MLAB_USERNAME_WEBDEV; // get from environment
+  var password = process.env.MLAB_PASSWORD_WEBDEV;
+  connectionString = "mongodb://" + username + ":" + password;
+  connectionString += "@ds211265.mlab.com:11265/heroku_j4h7sqfq"; // use yours
+}
+// import mongoose library
+var mongoose = require("mongoose");
+
+// connect server with database
 var db = mongoose.connect(
-    connectionString,
-    { useNewUrlParser: true }
+  connectionString,
+  { useNewUrlParser: true }
 );
 
+// export this file
 module.exports = db;
