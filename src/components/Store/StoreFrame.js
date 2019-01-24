a7ec7af96f85d6c1c086252eefb4d5191f81d919
sheila mcdaniel <smcdaniel617@aol.com>
2019-01-24 15:57:41 -0500

    added size, style and mount dropdown to frame

--- src/components/Store/StoreFrame.js
+++ src/components/Store/StoreFrame.js
@@ -1,23 +1,91 @@
 import React, { Component } from "react";
 import { Dropdown } from "semantic-ui-react";
 
-  class StoreFrame extends Component {
+class StoreFrame extends Component {
+  frameColorOptions = [
+    {
+      key: "hand-finished-natural-oak",
+      value: "Hand-finished natural oak",
+      text: "Hand-finished natural oak"
+    },
+    {
+      key: "hand-finished-black",
+      value: "Hand-finished black",
+      text: "Hand-finished black"
+    },
+    {
+      key: "hand-finished-grey",
+      value: "Hand-finished grey",
+      text: "Hand-finished grey"
+    },
+    {
+    key: "satin-white",
+      value: "Satin white",
+      text: "Satin white"
+    },
+    {
+      key: "satin-black",
+      value: "Satin black",
+      text: "Satin black"
+    }
+  ];
 
-  frameOptions = [
+  frameSizeOptions = [
     {
-      key: "choice-1",
-      value: "Choice 1",
-      text: "Choice 1"
+      key: "5 x 7",
+      value: "5 x 7",
+      text: "5 x 7"
     },
     {
-      key: "choice-2",
-      value: "Choice 2",
-      text: "Choice 2"
+      key: "8 x 10",
+      value: "8 x 10",
+      text: "8 x 10"
     },
     {
-      key: "choice-3",
-      value: "Choice 3",
-      text: "Choice 3"
+      key: "8.5 x 11",
+      value: "8.5 x 11",
+      text: "8.5 x 11"
+    },
+    {
+      key: "11 x 14",
+      value: "11 x 14",
+      text: "11 x 14"
+    },
+    {
+      key: "16 x 20",
+      value: "16 x 20",
+      text: "16 x 20"
+    }
+  ];
+
+  frameStyleOptions = [
+    {
+      key: "slim",
+      value: "Slim",
+      text: "Slim"
+    },
+    {
+      key: "medium",
+      value: "Medium",
+      text: "Medium"
+    },
+    {
+      key: "wide",
+      value: "Wide",
+      text: "Wide"
+    }
+  ];
+
+  frameMountOptions = [
+    {
+      key: "yes",
+      value: "Yes",
+      text: "Yes"
+    },
+    {
+      key: "no",
+      value: "No",
+      text: "No"
     }
   ];
 
@@ -44,22 +112,64 @@ import { Dropdown } from "semantic-ui-react";
             />
           </div>
         </div>
-        <div style={{ paddingBottom: '10px' }} className="ui form">
-                <div className="field">
-                  <Dropdown
-                    placeholder="test"
-                    fluid
-                    name="test"
-                    multiple
-                    selection
-                    options={this.frameOptions}
-                    onChange={this.onDropdownInputChange}                
-        />
-      </div>
-      </div>
+        <div style={{ paddingBottom: "10px" }} className="ui form">
+          <div className="field">
+            <Dropdown
+              placeholder="Color"
+              fluid
+              name="test"
+              multiple
+              selection
+              options={this.frameColorOptions}
+              onChange={this.onDropdownInputChange}
+            />
+          </div>
+        </div>
+
+        <div style={{ paddingBottom: "10px" }} className="ui form">
+          <div className="field">
+            <Dropdown
+              placeholder="Size"
+              fluid
+              name="test"
+              multiple
+              selection
+              options={this.frameSizeOptions}
+              onChange={this.onDropdownInputChange}
+            />
+          </div>
+        </div>
+
+        <div style={{ paddingBottom: "10px" }} className="ui form">
+          <div className="field">
+            <Dropdown
+              placeholder="Style"
+              fluid
+              name="test"
+              multiple
+              selection
+              options={this.frameStyleOptions}
+              onChange={this.onDropdownInputChange}
+            />
+          </div>
+        </div>
+
+        <div style={{ paddingBottom: "10px" }} className="ui form">
+          <div className="field">
+            <Dropdown
+              placeholder="Mount"
+              fluid
+              name="test"
+              multiple
+              selection
+              options={this.frameMountOptions}
+              onChange={this.onDropdownInputChange}
+            />
+          </div>
+        </div>
       </div>
     );
   }
-  }
+}
 
 export default StoreFrame;
