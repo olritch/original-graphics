9e13c89b323b739d2f035fa2a2c90bec2de0bdac
Olritch Germain <olritcholritch@gmail.com>
2019-01-28 15:11:36 -0500

    build for original graphic

--- server/services/contact.service.server.js
+++ server/services/contact.service.server.js
@@ -7,7 +7,7 @@ module.exports = function(app) {
     const DOMAIN = process.env.MAILGUN_DOMAIN;
 
     const mailgun = require("mailgun-js")({ apiKey: API_KEY, domain: DOMAIN });
-    const adminEmail = 'originalgraphics.contact@gmail.com'
+    const adminEmail = 'original.graphics@yahoo.com'
 
     const data = req.body;
     console.log(data)
