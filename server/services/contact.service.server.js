f16d2f6c80c11bb57daff83d3a88d5717af8e578
Olritch Germain <olritcholritch@gmail.com>
2019-01-28 15:53:29 -0500

    adding one more email"

--- server/services/contact.service.server.js
+++ server/services/contact.service.server.js
@@ -7,14 +7,15 @@ module.exports = function(app) {
     const DOMAIN = process.env.MAILGUN_DOMAIN;
 
     const mailgun = require("mailgun-js")({ apiKey: API_KEY, domain: DOMAIN });
-    const adminEmail = 'original.graphics@yahoo.com' + 'originalgraphics2019@gmail.com'
+    const adminEmail = 'original.graphics@yahoo.com';
+    const adminEmail2 = 'originalgraphics2019@gmail.com'
 
     const data = req.body;
     console.log(data)
 
     const info = {
       from: `${data.firstName} ${data.lastName} <${data.email}>`,
-      to: `Original Graphics Admin <${adminEmail}>`,
+      to: `Original Graphics Admin <${adminEmail}>, test <${adminEmail2}>`,
       subject: `${data.firstName} ${data.lastName}'s Feedback on ${data.commentTopic}`,
       text: data.feedback
     };
