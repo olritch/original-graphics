8767210d9cac972f42486026af232cfa54bd1dd2
Olritch Germain <olritcholritch@gmail.com>
2019-01-28 15:37:43 -0500

    added an extra email

--- server/services/contact.service.server.js
+++ server/services/contact.service.server.js
@@ -7,7 +7,7 @@ module.exports = function(app) {
     const DOMAIN = process.env.MAILGUN_DOMAIN;
 
     const mailgun = require("mailgun-js")({ apiKey: API_KEY, domain: DOMAIN });
-    const adminEmail = 'original.graphics@yahoo.com'
+    const adminEmail = 'original.graphics@yahoo.com' + 'originalgraphics2019@gmail.com'
 
     const data = req.body;
     console.log(data)
